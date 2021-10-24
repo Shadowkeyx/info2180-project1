f1647dd197fbefb8b0c208723a5fe8885c9d0051
Bradley Henry <shadownine2010@hotmail.com>
2021-10-23 21:12:50 -0500

    JS Addition

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+
+function Mailprompt(){
+
+    let button = document.getElementsByClassName("email")[0];
+    button.setAttribute("onclick","myFunction");
+
+
+
+}
+
+function myFunction() {
+    document.getElementById("email").submit();
+  }
+  
